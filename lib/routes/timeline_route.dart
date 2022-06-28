9c02fc6e2c8ff019c51229d493499fa23992da3c
YutoUshijima <ushijima.yuto.38x@st.kyoto-u.ac.jp>
2022-06-28 15:18:09 +0900

    DB作った！タイムライン作成中(バグあり)

--- lib/routes/timeline_route.dart
+++ lib/routes/timeline_route.dart
@@ -1,13 +1,155 @@
+//import 'package:dinyary/routes/home_route.dart';
 import 'package:flutter/material.dart';
-import 'header.dart';
- 
-class TimeLine extends StatelessWidget { // <- (※1)
+//import 'header.dart';
+import 'NoteViewModel.dart';
+
+
+class TimeLine extends StatefulWidget {
+  const TimeLine({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<TimeLine> {
+  List<Map<String, dynamic>> _memo = [];
+
+  bool _isLoading = true;
+
+  void _refreshJournals() async {
+    final data = await NoteViewModel.getNotes();
+    setState(() {
+      _memo = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshJournals();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  void _showForm(int? id) async {
+    if (id != null) {
+      final existingJournal =
+          _memo.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                top: 15,
+                left: 15,
+                right: 15,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Title'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(hintText: 'Description'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (id == null) {
+                        await _addItem();
+                      }
+
+                      if (id != null) {
+                        await _updateItem(id);
+                      }
+                      _titleController.text = '';
+                      _descriptionController.text = '';
+
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(id == null ? 'Create New' : 'Update'),
+                  )
+                ],
+              ),
+            ));
+  }
+
+  Future<void> _addItem() async {
+    await NoteViewModel.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  Future<void> _updateItem(int id) async {
+    await NoteViewModel.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  void _deleteItem(int id) async {
+    await NoteViewModel.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Successfully deleted a journal!'),
+    ));
+    _refreshJournals();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(),
-      body: Center(child: Text("タイムライン") // <- (※3)
-          ),
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _memo.length,
+              itemBuilder: (context, index) => Card(
+                color: Colors.deepPurple[200],
+                margin: const EdgeInsets.all(15),
+                child: ListTile(
+                    title: Text(_memo[index]['title']),
+                    subtitle: Text(_memo[index]['description']),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () => _showForm(_memo[index]['id']),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => _deleteItem(_memo[index]['id']),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _showForm(null),
+      ),
     );
   }
 }
