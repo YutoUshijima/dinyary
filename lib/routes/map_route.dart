a011d152466c2766c356871e5294703fd8414533
Wacky1425 <wackyhokuto.1425@gmail.com>
2022-07-05 16:18:29 +0900

    map_default

--- lib/routes/map_route.dart
+++ lib/routes/map_route.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'header.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; //googlemapのflutterにあるパッケージ
+import 'package:geolocator/geolocator.dart';
 
 class Map extends StatelessWidget {
   // <- (※1)
@@ -7,8 +10,39 @@ class Map extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(),
-      body: Center(child: Text("地図") // <- (※3)
-          ),
+      body: MapSample(),
     );
   }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      );
+  }
+
 }
