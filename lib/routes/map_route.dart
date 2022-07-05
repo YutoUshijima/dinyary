39ffe7e25e8e9ed99fad93d12d84d16e9a72fb2f
Wacky1425 <wackyhokuto.1425@gmail.com>
2022-07-05 18:16:56 +0900

    map 現在地関連

--- lib/routes/map_route.dart
+++ lib/routes/map_route.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'header.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart'; //googlemapのflutterにあるパッケージ
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // googlemapのflutterにあるパッケージ
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class Map extends StatelessWidget {
+class Map_view extends StatelessWidget {
   // <- (※1)
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // statelessなヘッダーとボディの部分だけ作成する部分
     return Scaffold(
       appBar: Header(),
       body: MapSample(),
@@ -15,34 +17,83 @@ class Map extends StatelessWidget {
   }
 }
 
-class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+class MapSample extends HookWidget {
+  final Completer<GoogleMapController> _mapController = Completer();
+  // 現在地が取得出来ない場合の初期表示位置を百万遍に設定
+  final Position _initialPosition = Position(
+    latitude: 35.02887415753032,
+    longitude: 135.77922493865512,
+    timestamp: DateTime.now(),
+    altitude: 0,
+    accuracy: 0,
+    heading: 0,
+    speed: 0,
+    speedAccuracy: 0,
   );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      );
+    // 初期表示座標のMarkerを設定
+    final initialMarkers = {
+      _initialPosition.timestamp.toString(): Marker(
+        markerId: MarkerId(_initialPosition.timestamp.toString()),
+        position: LatLng(_initialPosition.latitude, _initialPosition.longitude),
+      ),
+    };
+    // 現在地取得のためのpositionとmakerをhookのuseStateを使って設定
+    final position = useState<Position>(_initialPosition);
+    final markers = useState<Map<String, Marker>>(initialMarkers);
+
+    _setCurrentLocation(position, markers);
+    _animateCamera(position);
+
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal, // 航空写真などに変更する場合はこの部分をいじる
+        myLocationButtonEnabled: true, // GoogleMap の右下に表示される現在地へ移動するボタン
+        // 初期表示位置は百万遍に設定
+        initialCameraPosition: CameraPosition(
+          target: LatLng(_initialPosition.latitude, _initialPosition.longitude),
+          zoom: 14.4746,
+        ),
+        onMapCreated: _mapController.complete,
+        markers: markers.value.values.toSet(),
+      ),
+    );
   }
 
+  // 現在地を取得して状態を管理する
+  Future<void> _setCurrentLocation(ValueNotifier<Position> position,
+      ValueNotifier<Map<String, Marker>> markers) async {
+    final currentPosition = await Geolocator.getCurrentPosition( // 位置情報の取得許可をとる
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    const decimalPoint = 3;
+    // 過去の座標と最新の座標の小数点第三位で切り捨てた値を判定(更新頻度を落とせるようにするため)
+    if ((position.value.latitude).toStringAsFixed(decimalPoint) !=
+            (currentPosition.latitude).toStringAsFixed(decimalPoint) &&
+        (position.value.longitude).toStringAsFixed(decimalPoint) !=
+            (currentPosition.longitude).toStringAsFixed(decimalPoint)) {
+      // 現在地座標にMarkerを立てる
+      final marker = Marker(
+        markerId: MarkerId(currentPosition.timestamp.toString()),
+        position: LatLng(currentPosition.latitude, currentPosition.longitude),
+      );
+      markers.value.clear();
+      markers.value[currentPosition.timestamp.toString()] = marker;
+      // 現在地座標のstateを更新する
+      position.value = currentPosition;
+    }
+  }
+
+  Future<void> _animateCamera(ValueNotifier<Position> position) async {
+    final mapController = await _mapController.future;
+    // 現在地座標が取得できたらカメラを現在地に移動する
+    await mapController.animateCamera(
+      CameraUpdate.newLatLng(
+        LatLng(position.value.latitude, position.value.longitude),
+      ),
+    );
+  }
 }
