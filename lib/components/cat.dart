226676bba0872acb994eef6b4d6c70e4bc8c6c83
9rek1 <9rek199@gmail.com>
2022-07-12 17:24:19 +0900

    feat: 操作ボタンと鳴き声を追加

--- lib/components/cat.dart
+++ lib/components/cat.dart
@@ -1,10 +1,30 @@
-import 'package:flame/components.dart';
+import 'dart:math';
 
-class Cat extends SpriteComponent with HasGameRef {
-  Cat() : super(size: Vector2.all(100));
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
+import 'package:flame_audio/flame_audio.dart';
+
+List<String> catAudio = [
+  'cat1a.mp3',
+  'cat1b.mp3',
+  'cat3c.mp3',
+];
+
+class Cat extends SpriteComponent with HasGameRef, TapCallbacks {
+  Cat(this.spriteName, this.posX, this.posY) : super(size: Vector2.all(100));
+
+  String spriteName;
+  double posX, posY;
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    sprite = await gameRef.loadSprite('cat_mikeneko2.png');
+    sprite = await gameRef.loadSprite(spriteName);
+    position = Vector2(posX, posY);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    FlameAudio.play(catAudio[Random().nextInt(catAudio.length)]);
+    super.onTapDown(event);
   }
 }
--- lib/routes/game_route.dart
+++ lib/routes/game_route.dart
@@ -1,5 +1,7 @@
+import 'package:flame/experimental.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame_audio/flame_audio.dart';
 
 import 'header.dart';
 import '../components/world.dart';
@@ -13,25 +15,88 @@ class Game extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(),
-      body: GameWidget(game: game),
-    );
+        appBar: Header(),
+        body: Stack(children: [
+          GameWidget(game: game),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color(0x88ffffff),
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: IconButton(
+                onPressed: game.moveCameraToLeft,
+                icon: const Icon(Icons.keyboard_arrow_left),
+                iconSize: 40,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color(0x88ffffff),
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: IconButton(
+                onPressed: game.moveCameraToRight,
+                icon: const Icon(Icons.keyboard_arrow_right),
+                iconSize: 40,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color(0x88ffffff),
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: IconButton(
+                onPressed: game.addCat,
+                icon: const Icon(Icons.add),
+                iconSize: 30,
+              ),
+            ),
+          ),
+        ]));
   }
 }
 
-class GameRoot extends FlameGame {
-  final World _world = World();
-  final Cat _cat = Cat();
+class GameRoot extends FlameGame with HasTappableComponents {
+  final CatWorld _world = CatWorld();
+  int _numCat = 0;
+  final List<Cat> _cats = [
+    Cat('cat_mikeneko2.png', 250, 400),
+    Cat('cat_mikeneko2.png', 400, 400),
+  ];
 
   @override
   Future<void> onLoad() async {
     await add(_world);
-    add(_cat);
 
-    _cat.position = Vector2(250, 400);
-    camera.followComponent(
-      _cat,
-      worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y),
-    );
+    camera.worldBounds = Rect.fromLTRB(0, 0, _world.size.x, _world.size.y);
+    camera.speed = 100;
+
+    await FlameAudio.audioCache.loadAll(catAudio);
+  }
+
+  void moveCameraToLeft() {
+    camera.moveTo(Vector2(0, 0));
+  }
+
+  void moveCameraToRight() {
+    camera.moveTo(Vector2(_world.size.x, 0));
+  }
+
+  void addCat() {
+    if (_cats.length > _numCat) {
+      add(_cats[_numCat]);
+      _numCat++;
+    }
   }
 }
